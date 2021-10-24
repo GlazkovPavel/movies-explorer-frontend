876a42c685368b5f7a5f9c2dcfde3725f2648a4b
GlazkovPavel <glazprom@mail.ru>
2021-10-24 15:32:46 +0300

    feat: сделал верстку промо и о проекте

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,7 +3,7 @@ import './AboutProject.css';
 export function AboutProject() {
   return (
     <section className="about-project">
-      <h2 className="about-project__heading section__heading">О проекте</h2>
+      <h2 className="about-project__heading section__text">О проекте</h2>
       <div className="about-project__info">
         <div className="about-project__info_card">
           <h3 className="about-project__info-heading">Дипломный проект включал 5 этапов</h3>
@@ -15,8 +15,8 @@ export function AboutProject() {
         </div>
       </div>
       <div className="about-project__steps">
-        <p className="about-project__steps-text about-project__steps-text_type_one-week">1 неделя</p>
-        <p className="about-project__steps-text about-project__steps-text_type_four-week">4 недели</p>
+        <p className="about-project__steps-text about-project__text_one-week">1 неделя</p>
+        <p className="about-project__steps-text about-project__text_four-week">4 недели</p>
         <p className="about-project__description-text about-project__description-text-back">Back-end</p>
         <p className="about-project__description-text about-project__description-text-front">Front-end</p>
       </div>
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Switch } from 'react-router-dom';
-import '../../App.css';
+import './App.css';
 import {Header} from "../Header/Header";
 import {Main} from "../Main/Main";
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="page">
       <Main />
 
     </div>
