0a4af2cab730094573316485930edc9c76445c54
GlazkovPavel <glazprom@mail.ru>
2021-11-01 12:31:28 +0300

    feat: верстка profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -14,16 +14,19 @@ export function Profile() {
         <h2 className='profile__title'>Привет, {name}!</h2>
         <form className="profile__form">
           <fieldset className="profile__fields">
-            <div className="profile__container-name">
-              <input type="text" className="profile__input-name" value={name} placeholder="Имя"/>
+            <div className="profile__container">
+              <p className="profile__form-input-name">Имя</p>
+              <input type="text" className="profile__form-input" value={name}/>
             </div>
-            <div className="profile__container-email">
-              <input type="text" className="profile__input-email" value={email} placeholder="Email"/>
+            <hr/>
+            <div className="profile__container">
+              <p className="profile__form-input-name">E-mail</p>
+              <input type="text" className="profile__form-input" value={email}/>
             </div>
           </fieldset>
+          <button className="profile__button profile__button_type_edit">Редактировать</button>
+          <button className="profile__button profile__button_type_save">Сохранить</button>
         </form>
-        <button className="profile__button profile__button_type_edit">Редактировать</button>
-        <button className="profile__button profile__button_type_save">Сохранить</button>
         <button className="profile__button profile__button_type_signout">Выйти из аккаунта</button>
 
 
